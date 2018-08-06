a5bc96d87103254ec209aecc9026c8aa917479ff
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-08-06 19:00:35 +0200

    Start adding tests
    These fail right now because some strings contain null bytes.

--- HashTest.c
+++ HashTest.c
@@ -18,31 +18,86 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 #include "GeneralHashFunctions.h"
 
+void foo()
+{
+  /* unsigned char x = 128; */
+  /* char y = 128; */
+  /* signed char z = 128; */
+  /* char w = 127; */
+  /* char str[] = {w + 1, 0}; */
+  /* printf("%x %x %x %x\n", x, y, z, w + 1); */
+}
+
+typedef unsigned long ulong;
+
+void testWithLen(const unsigned char *key, size_t len, ulong expected64)
+{
+  printf("Key:                           %s\n", key             );
+  /* /\* printf(" 1. RS-Hash Function Value:    %x\n", RSHash   (key,len)); *\/ */
+  /* /\* printf(" 2. JS-Hash Function Value:    %x\n", JSHash   (key,len)); *\/ */
+  unsigned h1 = PJWHash  (key,len);
+  assert (h1 == HashPJW (key));
+  unsigned long h2 = PJWHash64(key,len);
+  assert (h2 == HashPJW64(key));
+
+  printf(" 3. PJW-Hash Function Value:   %x\n", h1);
+  printf(" 4. PJW64-Hash Function Value: %lx\n", h2);
+
+  assert (h2 == expected64);
+  /* /\* printf(" 5. ELF-Hash Function Value:   %x\n", ELFHash  (key,len)); *\/ */
+  /* /\* printf(" 6. BKDR-Hash Function Value:  %x\n", BKDRHash (key,len)); *\/ */
+  /* /\* printf(" 7. SDBM-Hash Function Value:  %x\n", SDBMHash (key,len)); *\/ */
+  /* /\* printf(" 8. DJB-Hash Function Value:   %x\n", DJBHash  (key,len)); *\/ */
+  /* /\* printf(" 9. DEK-Hash Function Value:   %x\n", DEKHash  (key,len)); *\/ */
+  /* /\* printf("10. BP-Hash Function Value:    %x\n", BPHash   (key,len)); *\/ */
+  /* /\* printf("11. FNV-Hash Function Value:   %x\n", FNVHash  (key,len)); *\/ */
+  /* /\* printf("12. AP-Hash Function Value:    %x\n", APHash   (key,len)); *\/ */
+}
+
+void test(const unsigned char *key, ulong expected64)
+{
+  int len = strlen((const char *) key);
+  testWithLen(key, len, expected64);
+}
 
 int main(int argc, char* argv[])
 {
-   char* key = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const char* key = "abcdefghijklmnopqrstuvwxyz1234567890";
+  foo();
 
-   printf("General Purpose Hash Function Algorithms Test\n");
-   printf("By Arash Partow - 2002          \n"                 );
-   printf("Key:                           %s\n", key             );
-   printf(" 1. RS-Hash Function Value:    %x\n", RSHash   (key,36));
-   printf(" 2. JS-Hash Function Value:    %x\n", JSHash   (key,36));
-   printf(" 3. PJW-Hash Function Value:   %x\n", PJWHash  (key,36));
-   printf(" 3. PJW-Hash Function Value again:   %x\n", HashPJW (key));
-   printf(" 4. PJW64-Hash Function Value: %lx\n",PJWHash64(key,36));
-   printf(" 4. PJW64-Hash Function Value again:   %lx\n", HashPJW64(key));
-   printf(" 5. ELF-Hash Function Value:   %x\n", ELFHash  (key,36));
-   printf(" 6. BKDR-Hash Function Value:  %x\n", BKDRHash (key,36));
-   printf(" 7. SDBM-Hash Function Value:  %x\n", SDBMHash (key,36));
-   printf(" 8. DJB-Hash Function Value:   %x\n", DJBHash  (key,36));
-   printf(" 9. DEK-Hash Function Value:   %x\n", DEKHash  (key,36));
-   printf("10. BP-Hash Function Value:    %x\n", BPHash   (key,36));
-   printf("11. FNV-Hash Function Value:   %x\n", FNVHash  (key,36));
-   printf("12. AP-Hash Function Value:    %x\n", APHash   (key,36));
+  printf("General Purpose Hash Function Algorithms Test\n");
+  printf("By Arash Partow - 2002          \n"                 );
+  test((const unsigned char *)key, 0x34545c16020230L);
+  /* char w = 127; */
+  /* char str[] = {w + 1, 0}; */
+  test((const unsigned char []) {0}, 0);
+  test((const unsigned char []) {0, 0}, 0);
+  test((const unsigned char []) {1, 0}, 1);
+  test((const unsigned char []) {127, 0}, 127);
+  test((const unsigned char []) {-128, 0}, 128);
+  test((const unsigned char []) {1, 1, 0}, 0x101L);
+  test((const unsigned char []) {1, 1, 1, 0}, 0x10101L);
+  test((const unsigned char []) {1, 1, 1, 1, 0}, 0x1010101l);
+  test((const unsigned char []) {1, 1, 1, 1, 1, 0}, 0x101010101L);
+  test((const unsigned char []) {2, 2, 2, 2, 2, 0}, 0x202020202L);
+  test((const unsigned char []) {1, 1, 1, 1, 1, 1, 0}, 0x10101010101L);
+  test((const unsigned char []) {1, 1, 1, 1, 1, 1, 1, 0}, 0x1010101010101L);
+  test((const unsigned char []) {1, 1, 1, 1, 1, 1, 1, 1, 0}, 0x1010101010001L);
+  unsigned char inp[1025];
+  inp[1024] = 0;
+  for (int i = 0; i < 1024; i++) {
+    inp[i] = 1;
+  }
+  test(inp, 0x1010101L);
+  for (int i = 0; i < 1024; i++) {
+    inp[i] = (unsigned char)i;
+  }
+  testWithLen(inp, 1024, 0x55aa01fe55ab54ffL);
 
-   return 1;
+  return 0;
 }
